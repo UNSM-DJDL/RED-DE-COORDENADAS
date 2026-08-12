9bbf0d15cf4848fca2bb1202211d3dbd12d7d035
UNSM-DJDL <ingeunsm@gmail.com>
2026-08-12 17:56:13 -0500

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,1 @@
-/* Archivo de Mapa Base */
-document.write('<link rel="stylesheet" href="https://cloudflare.com">');
-document.write('<script src="https://cloudflare.com"></script>');
-document.write('<script src="https://cloudflare.com"></script>');
+document.write('<script src="https://jsdelivr.net"></script>');
