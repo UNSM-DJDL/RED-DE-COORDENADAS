dd47cdff2c5625a1ea1e0e5bbe412c6a93c7a2c2
UNSM-DJDL <ingeunsm@gmail.com>
2026-08-12 17:51:59 -0500

    Create leaflet.js

---
+++ leaflet.js
@@ -0,0 +1,4 @@
+/* Archivo de Mapa Base */
+document.write('<link rel="stylesheet" href="https://cloudflare.com">');
+document.write('<script src="https://cloudflare.com"></script>');
+document.write('<script src="https://cloudflare.com"></script>');
