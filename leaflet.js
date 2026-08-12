1518fc89bd2c475a61f01d0324342818903b53cf
UNSM-DJDL <ingeunsm@gmail.com>
2026-08-12 18:02:02 -0500

    leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,3 +1,3 @@
-var s1 = document.createElement('script');
-s1.src = 'https://cloudflare.com';
-document.head.appendChild(s1);
+var sc1 = document.createElement('script');
+sc1.src = atob('aHR0cHM6Ly91bnBrZy5jb20vbGVhZmxldEAxLjkuNC9kaXN0L2xlYWZsZXQuanM=');
+document.head.appendChild(sc1);
